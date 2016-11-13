623eca562513ca7b83dc2a4068523f4ff18f0d84
Nic Lentzos <nicholaslentzos@gmail.com>
2016-11-13 18:06:33 +0000

    Chapter 22 NerdLauncher p.387 - List of packages from PackageManager.

--- app/src/main/java/com/lentzos/nic/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/lentzos/nic/nerdlauncher/NerdLauncherFragment.java
@@ -1,12 +1,18 @@
 package com.lentzos.nic.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by Nic on 13/11/2016.
@@ -16,6 +22,9 @@ import android.view.ViewGroup;
     //Override onCreateView() to stash a reference to the RecyclerView object mRecyclerView in a member variable.
 
 public class NerdLauncherFragment extends Fragment {
+    //To log the number of activities that PackageManager returns (for now).
+    private static final String TAG = "NerdLauncherFragment";
+
     private RecyclerView mRecyclerView;
 
     public static NerdLauncherFragment newInstance() {
@@ -27,6 +36,23 @@ public class NerdLauncherFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
         mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //Call setupAdapter() to create a RecyclerView.Adapter instance and set it on the RecyclerView object.
+        //Firstly it will just generate a list of application data.
+        //Create an implicit intent and get a list of activities that match the intent from PackageManager.
+        setupAdapter();
         return v;
+    }
+    //Call setupAdapter() to create a RecyclerView.Adapter instance and set it on the RecyclerView object.
+    //Firstly it will just generate a list of application data.
+    //Create an implicit intent and get a list of activities that match the intent from PackageManager.
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
     }
 }
